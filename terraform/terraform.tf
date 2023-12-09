c9edd25c0e7b02df243ff129c064e14912c50393
Jan Nemcik <janynemcik5@gmail.com>
2023-12-09 01:48:13 +0100

    added s3 backend for state

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -9,4 +9,12 @@ terraform {
       version = "2.4.0"
     }
   }
+  backend "s3" {
+    bucket = "powerex-terraform"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+    assume_role = {
+      role_arn = "arn:aws:iam::795211112650:role/terraform-state"
+    }
+  }
 }
